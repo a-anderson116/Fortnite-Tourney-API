512283889ae59b82e69e7e62d53ecff49a5af7ea
Alex Anderson <andersonaj116@gmail.com>
2023-02-28 09:36:37 -0600

    Initial Commit

---
+++ Fortnite Tourney API/Fortnite_Tourney_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fortnite_Tourney_APIApp.swift
+//  Fortnite Tourney API
+//
+//  Created by Alex Anderson on 2/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Fortnite_Tourney_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
